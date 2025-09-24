391f0e767abb617d956cead0f48a8a5ec2c1fd55
Amanda grunwell <amandagrunwell24@gmail.com>
2025-09-24 02:39:39 -0700

    {+}

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,6 @@ export default {
   schema: "./lib/db/schema.ts", // your schema
   out: "./drizzle", // migrations folder
   dialect: "postgresql",
-  // Just give drizzle-kit a full connection URL
   dbCredentials: {
     url: process.env.DATABASE_URL || "",
   },
--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,15 +1,9 @@
-// src/db/index.ts
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+
 import * as schema from "./schema";
-import { readFileSync } from "fs";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: true,
-    ca: process.env.RDS_CA_BUNDLE,
-  },
 });
-
 export const db = drizzle(pool, { schema });
